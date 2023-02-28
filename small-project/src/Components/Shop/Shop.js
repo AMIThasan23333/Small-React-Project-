954bb46a4e23ca4e761424b8041eeedb8ffc18eb
My Name <cpgsom819@gmail.com>
2023-03-01 01:21:11 +0800

    data loaded

--- small-project/src/Components/Shop/Shop.js
+++ small-project/src/Components/Shop/Shop.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
 const Shop = () => {
+
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+
+    },[])
+
     return (
         <div className='shop-contianer'>
            
                  <div className="products-container">
 
-                    <h3>This For Product</h3>
+                    <h3>This For Product: {products.length} </h3>
 
                  </div>
                  <div className="shop-container">
