8244924e83f3a7f505ffa8dd68b15c0142647519
My Name <cpgsom819@gmail.com>
2023-03-01 00:46:46 +0800

    NAVBAR FINISHED

--- small-project/src/App.js
+++ small-project/src/App.js
@@ -1,43 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
-import Country from './Countries/Country';
+import Header from './Components/Header/Header';
+// import Country from './Countries/Country';
 
 
 function App() {
   return (
-    <div className="App">
-      
-      <LoadCountries></LoadCountries>
-
-      <Country></Country>
+    <div>
+   
+     <Header></Header>
 
     </div>
   );
 }
 
 
-
-  function LoadCountries(){
-
-
-const [countries, setCountries] = useState([])
-
-useEffect( () => {
-
-  fetch('https://restcountries.com/v3.1/all')
-  .then(res => res.json())
-  .then(data => setCountries(data))
-
-}, [])
-
-    return (
-
-      <div>
-        <h1>Visting Every Country oF THE WORLD </h1>
-        <h3>Available Countries {countries.length}</h3>
-      </div>
-    )
-  }
 
 export default App;
