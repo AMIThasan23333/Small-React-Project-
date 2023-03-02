97a10912287b4d72721975bad34537e4794c0180
My Name <cpgsom819@gmail.com>
2023-03-02 11:26:59 +0800

    cart added

--- small-project/src/Components/Shop/Shop.js
+++ small-project/src/Components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from './../Product/Product';
+import Cart from '../Cart/Cart';
 const Shop = () => {
 
 
@@ -37,11 +38,9 @@ const Shop = () => {
                   }
 
                  </div>
-                 <div className="shop-container">
+                 <div className="cart-container">
         
-                  <h2>Order Summary </h2>
-
-                  <p>Selected Items  : {cart.length}</p>
+                  <Cart cart={cart}></Cart>
 
                  </div>
 
