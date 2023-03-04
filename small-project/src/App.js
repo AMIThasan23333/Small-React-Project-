7b5a8c205ab03d9ca138272a75b4c60a3b1b982f
My Name <cpgsom819@gmail.com>
2023-03-05 01:22:02 +0800

    loading data

--- small-project/src/App.js
+++ small-project/src/App.js
@@ -22,6 +22,7 @@ function App() {
       children : [
         {
           path : '/',
+          loader: () => fetch('products.json'),
           element: <Shop></Shop>
         },
         {
---
+++ small-project/src/Components/Loader/ProductAndCartLoader.js
@@ -0,0 +1,20 @@
+import { getStoredCart } from "../../utilities/fakedb";
+
+
+
+export const productAndCartLoader = async () => {
+
+
+    const productsData = await fetch('products.json');
+
+    const products = await productsData.json();
+
+
+    const savedCart = getStoredCart();
+
+
+
+    return products;
+
+
+}
